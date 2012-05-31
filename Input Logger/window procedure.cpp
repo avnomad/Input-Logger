af36331f9b6f216226eca51ecb9cedf40be4a530
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-05-31 03:29:52 +0300

    Added copyright notices and license.

--- Input Logger/window procedure.cpp
+++ Input Logger/window procedure.cpp
@@ -1,3 +1,21 @@
+//	Copyright (C) 2011-2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
+/*
+ *	This file is part of Input Logger.
+ *
+ *	Input Logger is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Input Logger is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Input Logger.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "window procedure.h"
 #include <WindowsX.h>
 
@@ -15,8 +33,8 @@ LRESULT CALLBACK windowProcedure(HWND window,UINT message,WPARAM argW,LPARAM arg
 	case WM_CREATE:
 		initializeInput(window);
 		return 0;
-	case WM_LBUTTONDOWN:	// normally I should make sure the capture is release by
-	case WM_MBUTTONDOWN:	// the same button that captures it... etc.
+	case WM_LBUTTONDOWN:	// normally I should make sure the capture is released by
+	case WM_MBUTTONDOWN:	// the same button that captured it... etc.
 	case WM_RBUTTONDOWN:
 		if(GetCapture() != window)
 		{
